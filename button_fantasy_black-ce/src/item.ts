61d51dd0a2ff54ee50d55948d2cd0e7263ed420e
Will Caine <william.caineiii@gmail.com>
2021-07-06 08:19:22 -0400

    initial commit

---
+++ button_fantasy_black-ce/src/item.ts
@@ -0,0 +1,55 @@
+export type Props = {
+  onClick?: Actions
+}
+
+export default class Button implements IScript<Props> {
+  clip = new AudioClip('sounds/click.mp3')
+  gtflShape = new GLTFShape('models/Black_Fantasy_Button.glb')
+
+  init() {}
+
+  play(entity: Entity) {
+    const source = new AudioSource(this.clip)
+    entity.addComponentOrReplace(source)
+    source.playing = true
+
+    const animator = entity.getComponent(Animator)
+    const clip = animator.getClip('trigger')
+    clip.stop()
+    clip.play()
+  }
+
+  spawn(host: Entity, props: Props, channel: IChannel) {
+    const button = new Entity()
+    button.setParent(host)
+
+    button.addComponent(this.gtflShape)
+
+    const animator = new Animator()
+    const clip = new AnimationState('trigger', { looping: false })
+    animator.addClip(clip)
+    button.addComponent(animator)
+
+    button.addComponent(
+      new OnPointerDown(
+        () => {
+          const pushAction = channel.createAction('push', {})
+          channel.sendActions([pushAction])
+        },
+        {
+          button: ActionButton.POINTER,
+          hoverText: 'Press',
+          distance: 6,
+        }
+      )
+    )
+
+    channel.handleAction('push', ({ sender }) => {
+      this.play(button)
+
+      if (sender === channel.id) {
+        channel.sendActions(props.onClick)
+      }
+    })
+  }
+}
