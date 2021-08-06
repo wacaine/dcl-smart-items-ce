bd30c53cc5bea93c53a7da0044d73e6bf360b3d3
Will Caine <william.caineiii@gmail.com>
2021-08-05 23:50:26 -0400

    add ability to define clickButton. also got changeHoverText working

--- button_fantasy_black-ce/src/item.ts
+++ button_fantasy_black-ce/src/item.ts
@@ -1,5 +1,6 @@
 export type Props = {
   onClick?: Actions
+  clickButton?: ActionButton,
   hoverTextEnabled?: string
   hoverTextDisabled?: string
   //hoverText?: string
@@ -8,7 +9,10 @@ export type Props = {
   onDisable?: Actions
   onEnable?: Actions
 }
-
+export type SetHoverText = {
+  // transition: number
+   hoverText: string
+}
 export default class Button implements IScript<Props> {
   clip = new AudioClip('sounds/click.mp3')
   gtflShape = new GLTFShape('models/Black_Fantasy_Button.glb')
@@ -27,18 +31,22 @@ export default class Button implements IScript<Props> {
     clip.play()
   }
   
-  toggle(entity: Entity, value: boolean, hoverText:string, makeClickable: boolean) {
+  toggle(transmitter: Entity, entity: Entity, value: boolean, hoverText:string, makeClickable: boolean) {
     if (this.active[entity.name] === value) return
 
-    const pointerDown = entity.getComponent(OnPointerDown)
-    if (hoverText !== null && hoverText !== undefined) {
-      pointerDown.hoverText=hoverText;
+    const pointerDown = entity.hasComponent(OnPointerDown) ? entity.getComponent(OnPointerDown) : null
+    if(pointerDown != null){
+      if (hoverText !== null && hoverText !== undefined) {
+        log("setting hovertext to " + hoverText + " for  " + transmitter.name)
+        pointerDown.hoverText=hoverText;
+      }
+      if(makeClickable !== null && makeClickable !== undefined){
+        pointerDown.showFeedback = makeClickable
+      }
     }
-    if(makeClickable !== null && makeClickable !== undefined){
-      pointerDown.showFeedback = makeClickable
+    if(value !== null && value !== undefined){
+      this.active[transmitter.name] = value
     }
-
-    this.active[entity.name] = value
   }
 
   isEnabled(entity: Entity) {
@@ -49,6 +57,7 @@ export default class Button implements IScript<Props> {
   spawn(host: Entity, props: Props, channel: IChannel) {
     const button = new Entity(host.name + '-button')
     const transmitter = host // or button?!?
+    const mainEntity = button // entity with onclick gtfl etc, primary "things"
     button.setParent(host)
 
     button.addComponent(this.gtflShape)
@@ -75,7 +84,7 @@ export default class Button implements IScript<Props> {
         channel.sendActions([pushAction])
       },
       {
-        button: ActionButton.POINTER,
+        button: props.clickButton ? props.clickButton : ActionButton.POINTER,
         hoverText: startingHoverText,
         distance: 6,
         showFeedback: enableClickable
@@ -83,21 +92,22 @@ export default class Button implements IScript<Props> {
     ) )
 
     channel.handleAction('disableClickable', (action) => {
-      this.toggle(transmitter,false,null,false)
+      this.toggle(transmitter,mainEntity,false,null,false)
     })
 
     channel.handleAction('enableClickable', (action) => {
-      this.toggle(transmitter,true,null,true)
+      this.toggle(transmitter,mainEntity,true,null,true)
     })
 
-    //channel.handleAction('setHoverText', (action) => {
-    //  const { hoverText } = action.values
-    //  this.toggle(transmitter,null,hoverText)
-    //})
+    channel.handleAction<SetHoverText>('setHoverText', (action) => {
+      const { hoverText } = action.values
+      log("hoverText called " + hoverText + " transmitter " + transmitter.name + " vs " + action.entityName)
+      this.toggle(transmitter,mainEntity,null,hoverText,null)
+    })
 
     channel.handleAction('deactivate', (action) => {
       const { sender } = action
-      this.toggle(transmitter,false,hoverTextDisabled,null)
+      this.toggle(transmitter,mainEntity,false,hoverTextDisabled,null)
       if (sender === channel.id) {
           channel.sendActions(props.onDisable)
       }
@@ -105,21 +115,24 @@ export default class Button implements IScript<Props> {
     
     channel.handleAction('activate', (action) => {
       const { sender } = action
-      this.toggle(transmitter,true,hoverTextEnabled,null)
+      this.toggle(transmitter,mainEntity,true,hoverTextEnabled,null)
       if (sender === channel.id) {
           channel.sendActions(props.onEnable)
       }
     })
 
-    channel.handleAction('toggle', ({ sender }) => {
+    channel.handleAction('toggle', (action) => {
+      const { sender } = action
       const newValue = !this.isEnabled(transmitter);
-      this.toggle(transmitter, newValue, newValue ? hoverTextEnabled : hoverTextDisabled, null)
+      this.toggle(transmitter,mainEntity, newValue, newValue ? hoverTextEnabled : hoverTextDisabled, null)
       if (sender === channel.id) {
         channel.sendActions(newValue ? props.onEnable : props.onDisable)
       }
     })
 
-    channel.handleAction('push', ({ sender }) => {
+    channel.handleAction('push', (action) => {
+      const { sender } = action
+      log("pushed called  transmitter " + transmitter.name + " vs " + action.entityName)
       if(this.isEnabled(transmitter)){
         this.play(button)
 
